aed60374183e269332795407934f44888ee55e63
zumo <zumo@solute.de>
2019-06-03 08:48:12 +0200

    register user and bcrypt

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
-const { MongoClient, ObjectID } = require('mongodb');
 const mongoose = require('mongoose');
 
 const app = express();
 
+// DB
+
+mongoose.connect('mongodb://127.0.0.1:27017/auth', { useNewUrlParser: true })
+    .then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err));
+
+
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,82 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 
 const router = express.Router();
 
+// User model
+const User = require('../models/User');
 
 // Login Route
 router.get('/login', (req, res) => res.render('login'));
 
 // Register Route
 router.get('/register', (req, res) => res.render('register'));
+
+// Register Handle
+router.post('/register', (req, res) => {
+    // eslint-disable-next-line object-curly-newline
+    const { name, email, password, password2 } = req.body;
+    const errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    if (errors.length > 0) {
+        res.render('register', {
+            errors,
+            name,
+            email,
+            password,
+            password2,
+        });
+    } else {
+        // Validation passed
+        User.findOne({ email })
+            .then((user) => {
+                if (user) {
+                    // User exists
+                    errors.push({ msg: 'Email is already registered' });
+                    res.render('register', {
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2,
+                    });
+                } else {
+                    const newUser = new User({
+                        name,
+                        email,
+                        password,
+                    });
+
+                    // Hash Password
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (error, hash) => {
+                            if (error) throw error;
+                            // Set password to hashed
+                            newUser.password = hash;
+                            // Save user
+                            newUser.save()
+                                .then((user) => {
+                                    res.redirect('/users/login');
+                                })
+                                .catch(e => console.log(e));
+                        });
+                    });
+                }
+            })
+            .catch(err => console.log(err));
+    }
+});
 
 module.exports = router;
